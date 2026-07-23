2abce40f20d9010a4386399c93a974cfe3e559be
Younes Benaggoun <younes.benaggoun3@gmail.com>
2026-07-23 20:06:47 +0200

    test(user): make register login role in same file

--- src/__tests__/unit/User_register_login_role.test.js
+++ src/__tests__/unit/User_register_login_role.test.js
@@ -4,7 +4,7 @@ import { describe, expect, afterAll } from "vitest";
 import request from "supertest";
 import app from "../../app.js";
 import UserModel from "../../3_InfraStructure/database/models/user.model.js";
-import  Role  from "../../1_Domain/entities/Role.js";
+import Role from "../../1_Domain/entities/Role.js";
 
 import mongoose from "mongoose";
 
@@ -122,11 +122,26 @@ describe("Register", () => {
         expect(res.statusCode).toBe(200);
     });
 
+    test("Organizer can access User Route and get List Of Users", async () => {
+        const res = await request(app)
+            .get("/User")
+            .set("Authorization", `Bearer ${organizerToken}`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.length).toBeGreaterThan(0);
+    });
+
     test("Exposer can't access organizer route", async () => {
         const res = await request(app)
             .post("/exposition/add")
             .set("Authorization", `Bearer ${exposerToken}`);
         expect(res.statusCode).toBe(403);
+    });
+    test("Exposer can not access User Route", async () => {
+        const res = await request(app)
+            .get("/User")
+            .set("Authorization", `Bearer ${exposerToken}`);
+        expect(res.statusCode).toBe(403);
+        expect(res.body.length).toBe(undefined);
     });
 
     test("Visitor can't access organizer route", async () => {
@@ -140,9 +155,7 @@ describe("Register", () => {
 
 
 
-
 afterAll(async () => {
-
     await UserModel.findByIdAndDelete(organizerId);
     await UserModel.findByIdAndDelete(exposerId);
     await UserModel.findByIdAndDelete(visitorId);
